ff18df4684d4e5cc31b82f6748aa7f0a34fe6886
Maulana Yusup <maulana.yusup8989@gmail.com>
2023-02-12 22:07:06 +0700

    Laravel MPC

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('container')
+<article class="mt-5">
+    <h2>{{$post['title']}}</h2>
+    <h5>by: {{$post['author']}}</h5>
+    <p>{{$post['content']}}</p>
+</article>
+    <a href="/posts">back to post</a>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home',[
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/posts',[PostController::class,'index']);
+
+Route::get('/about', function () {
+    return view('about',[
+        'title' =>'About',
+        'name' => 'Maulana Yusup',
+        'email' => 'maulana.yusup13@wastukancana.ac.id',
+        'img' => 'avatar.png'
+    ]);
+});
+
+//Halaman  Single Post
+Route::get('posts/{slug}',[PostController::class,'show']);
